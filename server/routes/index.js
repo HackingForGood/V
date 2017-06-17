f7912d791f27beb210b3e3360c9db5c0108ca617
alex cushing <alexander_cushing@student.uml.edu>
2017-06-17 16:22:34 -0400

    sign in sign up

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import * as AuthController from '../controllers/authController.js';
+import * as AuthController from '../controllers/AuthController.js';
 
 const router = express();
 
