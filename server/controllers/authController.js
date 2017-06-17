121a87595e0c0958aeb4bb6a187e3cb94c4e1a99
Thomas Tracy <Thomas_Tracy@student.uml.edu>
2017-06-17 11:06:24 -0400

    auth routes

---
+++ server/controllers/authController.js
@@ -0,0 +1,8 @@
+const Auth = {
+  
+  //stubby wubbys
+  login: (req, res) => res.send('login'),
+  register: (req, res) => res.send('register')
+}
+
+module.exports = Auth
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+app.use(require('./routes')); 
+
 app.get('/test', (req, res) => res.sendStatus(200));
+
+
+
 
 app.listen(8888, () => {
   console.log('listening');
